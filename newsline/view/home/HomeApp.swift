c312515d0a8b7392ff859eb241f4e64ae32d8eba
sheikh-20 <sheikhzs1032@gmail.com>
2024-07-14 17:59:37 +0530

    home bottom bar navigation integrated

---
+++ newsline/view/home/HomeApp.swift
@@ -0,0 +1,39 @@
+//
+//  HomeApp.swift
+//  newsline
+//
+//  Created by Sheikh Mohideen on 14/07/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeApp: View {
+    var body: some View {
+        TabView {
+            HomeView().tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            DiscoverView().tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Discover")
+            }
+            ProfileView().tabItem {
+                Image(systemName: "bookmark.fill")
+                Text("Bookmark")
+            }
+            BookmarkView().tabItem {
+                Image(systemName: "person.fill")
+                Text("Account")
+            }
+    
+        }
+    }
+}
+
+struct HomeApp_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeApp()
+    }
+}
