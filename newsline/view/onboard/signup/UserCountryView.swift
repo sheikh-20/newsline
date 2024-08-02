2861951c66bb692e772065970badf28854660b06
sheikh-20 <sheikhzs1032@gmail.com>
2024-08-02 17:07:46 +0530

    user country view added

--- newsline/view/onboard/signup/UserCountryView.swift
+++ newsline/view/onboard/signup/UserCountryView.swift
@@ -6,3 +6,82 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct UserCountryView: View {
+    @State private var text: String = ""
+    
+    var body: some View {
+        
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Where do you come from?")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding()
+                
+                Text("Select your country of origin. This will help us to make the best recommendation for you.")
+                    .font(.body)
+                    .padding(.horizontal)
+                
+                ZStack(alignment: .leading) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 1)
+                            .frame(height: 40)
+                        
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                                .padding(.leading, 8)
+                            
+                            TextField("Search Country", text: $text)
+                                .padding(.leading, 8) // Adjust padding to align text correctly
+                        }
+                        .padding(.horizontal)
+                    }
+                    .padding()
+                
+                List(CountryRepository.getCountries()) { country in
+                    HStack {
+                        HStack {
+                                           Text(country.flag)
+                                               .font(.largeTitle)
+                                           Text(country.name)
+                                               .font(.headline)
+                                       }
+                                       .padding(.vertical, 8)
+                    }
+                }
+                
+                Divider()
+                    .padding([.leading, .trailing, .bottom])
+                
+                HStack {
+                   
+                    Button(
+                        action: { },
+                        label: { Text("Continue").fontWeight(.semibold) })
+                        .buttonStyle(ElevatedButtonStyle())
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    
+                }.padding(.horizontal)
+                
+            }.toolbar {
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        // Action for trailing button
+                    }) {
+                        Image(systemName: "chevron.left")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct UserCountryView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserCountryView()
+    }
+}
