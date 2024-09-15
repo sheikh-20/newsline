ff29e0de71469b970689916805478fd52708b601
Anil@1679 <anilchoudhary1679@gmail.com>
2024-09-15 16:44:56 +0530

    profile view UI added

--- newsline/view/home/HomeView.swift
+++ newsline/view/home/HomeView.swift
@@ -22,6 +22,7 @@ struct HomeView: View {
                 Spacer()
                 
                    }
+            .navigationBarTitleDisplayMode(.inline)
             .navigationBarBackButtonHidden(true)
             .toolbar {
              
